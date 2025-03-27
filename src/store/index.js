28655a7a34b7dac18e02f6b8ce5dad0ad654f967
Mücahit <71331160+mucahitkalabalik@users.noreply.github.com>
2025-03-27 04:49:18 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,8 @@
 import { defineStore } from "pinia";
 
-// You can name the return value of `defineStore()` anything you want,
-// but it's best to use the name of the store and surround it with `use`
-// and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
-// the first argument is a unique id of the store across your application
 export const useGlobalStore = defineStore("global", {
   state: () => {
     return {
-      // all these properties will have their type inferred automatically
       data: [
         {
           id: 10,
